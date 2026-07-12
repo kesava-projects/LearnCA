5f0f16da82ece0cf0efa76a8d771f2de6c434898
kesava-projects <kesavaharshavardhan@gmail.com>
2026-07-12 17:05:13 +0530

    baseurl

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: '/api',
+  baseURL: process.env.VITE_API_URL || 'http://localhost:5001/api',
   withCredentials: true, // Send httpOnly cookies automatically
   headers: {
     'Content-Type': 'application/json',
